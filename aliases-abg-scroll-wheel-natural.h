e489ca820a62fcf660a8d80b5578faa16b406639
Andrew Gallagher <andrew.gallagher@siren.io>
2017-11-02 00:05:39 +0000

    Enable scroll wheel buttons in abg-function-inverted-t layer

---
+++ aliases-abg-scroll-wheel-natural.h
@@ -0,0 +1,6 @@
+/* Define aliases for a mouse wheel configured with "natural scrolling"
+ * As per xxv's commit ee7d2e963c8435d9f2f444a83777c4b43ca15a3b
+ */
+
+#define SCROLL_UP Key_mouseScrollDn
+#define SCROLL_DOWN Key_mouseScrollUp
