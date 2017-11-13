ff6d1e0e8faaa57cfb6507ba2a375917751d437c
Andrew Gallagher <andrew.gallagher@siren.io>
2017-11-13 11:05:53 +0000

    Added hundreds, thousands and comma keys to keypad layer

--- layer-abg-numpad-apple.h
+++ layer-abg-numpad-apple.h
@@ -26,11 +26,11 @@ KEYMAP_STACKED
    ___, ___, ___, ___,
    ___,
 
-   M(MACRO_VERSION_INFO),  ___,           Key_Clear,       Key_KeypadEquals,   Key_KeypadDivide,   Key_KeypadMultiply, ___,
-   ___,                    ___,           Key_Keypad7,     Key_Keypad8,        Key_Keypad9,        Key_KeypadSubtract, ___,
-                           Key_Backspace, Key_Keypad4,     Key_Keypad5,        Key_Keypad6,        Key_KeypadAdd,      ___,
-   ___,                    ___,           Key_Keypad1,     Key_Keypad2,        Key_Keypad3,        Key_KeypadEnter,    ___,
-   ___, ___, Key_Keypad0, Key_KeypadDot,
+   M(MACRO_VERSION_INFO),  ___,                 Key_Clear,       Key_KeypadEquals,   Key_KeypadDivide,   Key_KeypadMultiply, ___,
+   ___,                    ___,                 Key_Keypad7,     Key_Keypad8,        Key_Keypad9,        Key_KeypadSubtract, ___,
+                           Key_Backspace,       Key_Keypad4,     Key_Keypad5,        Key_Keypad6,        Key_KeypadAdd,      ___,
+   ___,                    Key_KeypadComma,     Key_Keypad1,     Key_Keypad2,        Key_Keypad3,        Key_KeypadEnter,    ___,
+   Key_Keypad000, Key_Keypad00, Key_Keypad0, Key_KeypadDot,
    ___)
 
 // *INDENT-ON*
