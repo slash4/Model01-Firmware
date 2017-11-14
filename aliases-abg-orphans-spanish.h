de276176eee4ce738f5e2770c1d306e07e25bac2
Andrew Gallagher <andrew.gallagher@siren.io>
2017-11-14 15:31:29 +0000

    Adjusted spanish orphans so that both spanish bracket pairs are colocated

--- aliases-abg-orphans-spanish.h
+++ aliases-abg-orphans-spanish.h
@@ -6,9 +6,9 @@
  * use one of the ABG Fn layers so that PgUp, PgDn and Num can be accessed.
  */
 
-#define LEFT_OF_A	Key_RightBracket
+#define LEFT_OF_A	Key_Equals
 #define LEFT_OF_Z	Key_NonUsBackslashAndPipe
 #define LEFT_OF_6	Key_Minus
-#define RIGHT_OF_0	Key_Equals
+#define RIGHT_OF_0	Key_RightBracket
 #define RIGHT_OF_P	Key_LeftBracket
 #define RIGHT_OF_SLASH	Key_Backslash
