faf844ac281e890065475674f026ac76c620e655
Andrew Gallagher <andrew.gallagher@siren.io>
2017-11-14 11:03:26 +0000

    Added orphans definitions

---
+++ aliases-abg-orphans-abg.h
@@ -0,0 +1,14 @@
+/*
+ * Arrange orphaned keys according to ABG QWERTY/Dvorak optimization
+ */
+
+/* Since this DOES NOT contain PgUp, PgDn or Num keys, it is recommended to
+ * use one of the ABG Fn layers so that PgUp, PgDn and Num can be accessed.
+ */
+
+#define LEFT_OF_A	Key_Backslash
+#define LEFT_OF_Z	Key_NonUsBackslashAndPipe
+#define LEFT_OF_6	Key_Minus
+#define RIGHT_OF_0	Key_Equals
+#define RIGHT_OF_P	Key_LeftBracket
+#define RIGHT_OF_SLASH	Key_RightBracket
---
+++ aliases-std-orphans.h
@@ -0,0 +1,10 @@
+/*
+ * Arrange orphaned keys according to stock layout
+ */
+
+#define LEFT_OF_A	Key_PageUp
+#define LEFT_OF_Z	Key_PageDown
+#define LEFT_OF_6	M(MACRO_ANY)
+#define RIGHT_OF_0	LockLayer(NUMPAD)
+#define RIGHT_OF_P	Key_Equals
+#define RIGHT_OF_SLASH	Key_Minus
